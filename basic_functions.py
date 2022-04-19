49821341e092665e4a51befeaee0a57976d6fde0
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 13:26:43 +0430

    blur image with gaussian kernel

--- basic_functions.py
+++ basic_functions.py
@@ -4,4 +4,7 @@ cv.imshow("original image", img)
 #convert RGB to gray scale 
 gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("gray scale image", gray_img)
+#blur image
+blurred_img=cv.GaussianBlur(gray_img, (3,3),cv.BORDER_DEFAULT)
+cv.imshow("blurred image",blurred_img)
 cv.waitKey(0)
