1d1297ae52d0fd76f77fb93af6e905c8fda3bacd
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 16:59:23 +0430

    translation

--- basic_functions.py
+++ basic_functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2 as cv
 img=cv.imread("G:\learn opencv/T.tif")
 cv.imshow("original image", img)
@@ -5,7 +6,7 @@ cv.imshow("original image", img)
 gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("gray scale image", gray_img)
 #blur image
-blurred_img=cv.GaussianBlur(gray_img, (3,3),cv.BORDER_DEFAULT)
+blurred_img=cv.GaussianBlur(gray_img, (3,3),cv.BORDER_DEFAULT)  #or use your own sigma not default 
 cv.imshow("blurred image",blurred_img)
 #edge detection
 canny=cv.Canny(gray_img,50,100)
@@ -17,9 +18,16 @@ cv.imshow("dilated image", dilated_img)
 eroded_img=cv.erode(dilated_img,(7,7),iterations=5)
 cv.imshow("eroded image",eroded_img)
 #scaling 
-resized_img=cv.resize(img,None,fx=2,fy=3) # or cv.resize(img,(500,500),interpolation=cv.INTER_AREA)
+resized_img=cv.resize(img,None,fx=0.2,fy=0.3) # or cv.resize(img,(500,500),interpolation=cv.INTER_AREA)
 cv.imshow("resized image",resized_img)
 #crop
 cropped_img=img[100:150,20:100]
 cv.imshow("cropped image",cropped_img)
+#translation 
+def translate(img,tx,ty): 
+    width,height = img.shape[:2]
+    trans_mx=np.float32([[1,0,tx],[0,1,ty],[0,0,1]])
+    return cv.warpPerspective(img,trans_mx, (width,height))
+translated_img=translate(img,20,50)
+cv.imshow("translated image",translated_img)
 cv.waitKey(0)
