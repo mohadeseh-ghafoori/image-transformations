4f0efb9308e4f5695272baa8f7ce0908554f70c6
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 13:54:52 +0430

    resize image

--- basic_functions.py
+++ basic_functions.py
@@ -16,4 +16,7 @@ cv.imshow("dilated image", dilated_img)
 #erode : erode thick edges(thin them)
 eroded_img=cv.erode(dilated_img,(7,7),iterations=5)
 cv.imshow("eroded image",eroded_img)
+#scaling 
+resized_img=cv.resize(img,None,fx=2,fy=3) # or cv.resize(img,(500,500),interpolation=cv.INTER_AREA)
+cv.imshow("resized image",resized_img)
 cv.waitKey(0)
