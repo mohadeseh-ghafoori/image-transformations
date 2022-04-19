24c691b7ccf5ee770120230dd1c8d012e2111fd0
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 13:18:18 +0430

    convert colorful image to a gray scale one

--- basic_functions.py
+++ basic_functions.py
@@ -1,4 +1,7 @@
 import cv2 as cv
 img=cv.imread("G:\learn opencv/T.tif")
 cv.imshow("original image", img)
+#convert RGB to gray scale 
+gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow("gray scale image", gray_img)
 cv.waitKey(0)
