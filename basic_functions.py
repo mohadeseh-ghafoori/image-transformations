9f6275aa5d39fe550ce7f03c9113b09778a9b496
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 13:38:13 +0430

    dilate edges

--- basic_functions.py
+++ basic_functions.py
@@ -10,4 +10,7 @@ cv.imshow("blurred image",blurred_img)
 #edge detection
 canny=cv.Canny(gray_img,50,100)
 cv.imshow("canny edge", canny)
+#dilate : thicken edges 
+dilated_img=cv.dilate(canny,(7,7),iterations=2)
+cv.imshow("dilated image", dilated_img)
 cv.waitKey(0)
